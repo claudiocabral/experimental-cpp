bc0482f1086621d1b4f3ea7223d79d1ed00b858e
Claudio Cabral <cabral1349@gmail.com>
2020-11-05 18:28:18 +0100

    add zip modifications

--- includes/zip/zip.h
+++ includes/zip/zip.h
@@ -1,91 +1,97 @@
 #pragma once
 
+#include <array>
+#include <limits>
 #include <tuple>
 #include <utility>
 
-namespace Akura
-{
-template <class... Args>
-struct Zip;
+namespace Akura {
+    template <class... Args> struct Zip;
 
-template <class... Args>
-Zip(Args&&...) -> Zip<Args...>;
+    template <class... Args> Zip(Args &&...) -> Zip<Args...>;
 
-template <class... Args>
-struct Iterator;
+    template <class... Args> struct Iterator;
 
-template <class T>
-struct Iterator<T>
-{
-    using Type = std::decay_t<T>;
-    using IteratorType = typename Type::iterator;
-    IteratorType it;
-    constexpr inline Iterator(IteratorType t) : it(t) {}
-    constexpr inline auto operator++()
-    {
-        ++it;
-        return this;
+    template <class T> struct Iterator<T> {
+        using Type = std::decay_t<T>;
+        using IteratorType = typename Type::iterator;
+        IteratorType it;
+        constexpr inline Iterator(IteratorType t) : it(t) {}
+        constexpr inline auto operator++() {
+            ++it;
+            return *this;
+        }
+        constexpr inline auto operator!=(const Iterator<T> &other) {
+            return it != other.it;
+        }
+        constexpr inline auto operator*() { return std::forward_as_tuple(*it); }
+    };
+
+    template <class T, class... Args>
+        struct Iterator<T, Args...> : public Iterator<Args...> {
+            using Type = std::decay_t<T>;
+            using IteratorType = typename Type::iterator;
+            IteratorType it;
+            constexpr inline Iterator(IteratorType t, Iterator<Args...> parent)
+                : Iterator<Args...>(parent), it(t){};
+            constexpr inline auto &operator++() {
+                ++it;
+                Iterator<Args...>::operator++();
+                return *this;
+            }
+            constexpr inline auto operator!=(const Iterator<T, Args...> &other) {
+                return it != other.it && Iterator<Args...>::operator!=(other);
+            }
+            constexpr inline auto operator*() {
+                return std::tuple_cat(std::forward_as_tuple(*it),
+                        Iterator<Args...>::operator*());
+            }
+        };
+
+    template <class T, class F>
+        constexpr inline auto iteratorGenerator(F &&f, T &&t) {
+            return Iterator<T>(f(std::forward<T>(t)));
+        }
+
+    template <class T, class F, class... Args>
+        constexpr inline auto iteratorGenerator(F &&f, T &&t, Args &&... args) {
+            return Iterator<T, Args...>(
+                    f(std::forward<T>(t)),
+                    iteratorGenerator(std::forward<F>(f), std::forward<Args>(args)...));
+        }
+
+    template <class... Args> struct Zip {
+        Iterator<Args...> _begin;
+        Iterator<Args...> _end;
+        Zip(Args &&... args)
+            : _begin(iteratorGenerator([](auto &&cont) { return cont.begin(); },
+                        std::forward<Args>(args)...)),
+            _end(iteratorGenerator([](auto &&cont) { return cont.end(); },
+                        std::forward<Args>(args)...)) {}
+
+        constexpr inline auto begin() { return _begin; }
+        constexpr inline auto end() { return _end; }
+    };
+
+struct IndexIterator {
+    int i = 0;
+    // IndexIterator() = default;
+    constexpr IndexIterator(int i) : i(i) {}
+    constexpr int size() const { return std::numeric_limits<int>::max(); }
+    using iterator = IndexIterator;
+    IndexIterator &operator++() {
+        ++i;
+        return *this;
     }
-    constexpr inline auto operator!=(const Iterator<T> &other)
-    {
-        return it != other.it;
+    int &operator*() {
+        // std::cout << "operator * " << i << std::endl;
+        return i;
     }
-    constexpr inline auto operator*()
-    {
-        return std::forward_as_tuple(*it);
-    }
+    bool operator!=(const IndexIterator &other) const { return i != other.i; }
+    constexpr auto &begin() const { return *this; }
+    constexpr auto end() const { return IndexIterator{size()}; }
 };
 
-template <class T, class... Args>
-struct Iterator<T, Args...> : public Iterator<Args...>
-{
-    using Type = std::decay_t<T>;
-    using IteratorType = typename Type::iterator;
-    IteratorType it;
-    constexpr inline Iterator(IteratorType t, Iterator<Args...> parent) : Iterator<Args...>(parent), it(t) {};
-    constexpr inline auto operator++()
-    {
-        ++it;
-        Iterator<Args...>::operator++();
-        return this;
-    }
-    constexpr inline auto operator!=(const Iterator<T, Args...> &other)
-    {
-        return it != other.it
-            && Iterator<Args...>::operator!=(other);
-    }
-    constexpr inline auto operator*()
-    {
-        return std::tuple_cat(std::forward_as_tuple(*it), Iterator<Args...>::operator*());
-    }
-};
+template <class T> auto enumerate(T &&t) { return Akura::Zip(t, IndexIterator{0}); }
 
-template <class T, class F>
-constexpr inline auto iteratorGenerator(F && f, T&& t)
-{
-    return Iterator<T>(f(t));
-}
-
-template <class T, class F, class... Args>
-constexpr inline auto iteratorGenerator(F &&f, T&& t, Args &&... args)
-{
-    return Iterator<T, Args...>(f(t), iteratorGenerator(f, args...));
-}
-
-
-template < class... Args>
-struct Zip
-{
-    Iterator<Args...> _begin;
-    Iterator<Args...> _end;
-    Zip(Args&&... args)  : _begin(iteratorGenerator([](auto & cont) { return cont.begin();}, args...)),
-            _end(iteratorGenerator([](auto & cont) { return cont.end();}, args...)) {}
-
-    constexpr inline auto begin() {
-        return _begin;
-    }
-    constexpr inline auto end() {
-        return _end;
-    }
-};
-};
+}; // namespace Akura
