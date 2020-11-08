bc62cc89fba07fe6e336669df3e2fe920555a966
Claudio Cabral <cabral1349@gmail.com>
2020-11-08 17:45:40 +0100

    format

--- includes/zip/zip.h
+++ includes/zip/zip.h
@@ -3,69 +3,70 @@
 #include <array>
 #include <limits>
 #include <tuple>
-#include <utility>
 #include <type_traits>
+#include <utility>
 
 namespace Akura {
-    template <class... Args>
-        struct Iterator {
-            Iterator(Args... args) : its(args...) {}
-            std::tuple<Args...> its;
-            constexpr inline auto &operator++() {
-                std::apply([](auto &&... args) { (++args, ...); }, its);
-                return *this;
-            }
-            constexpr inline auto operator==(const Iterator<Args...> &other) {
-                return std::apply([](Args &&... a, Args &&... b) {
-                        return ((a == b) || ... ); },
-                        std::tuple_cat(its, other.its));
-            }
-            constexpr inline auto operator!=(const Iterator<Args...> &other) {
-                return !(*this == other);
-            }
-            constexpr inline auto operator*() {
-                return std::apply([](auto &&... args) { return std::tuple(*args...); }, its);
-            }
-        };
+template <class... Args> struct Iterator {
+  Iterator(Args... args) : its(args...) {}
+  std::tuple<Args...> its;
+  constexpr inline auto &operator++() {
+    std::apply([](auto &&... args) { (++args, ...); }, its);
+    return *this;
+  }
+  constexpr inline auto operator==(const Iterator<Args...> &other) {
+    return std::apply(
+        [](Args &&... a, Args &&... b) { return ((a == b) || ...); },
+        std::tuple_cat(its, other.its));
+  }
+  constexpr inline auto operator!=(const Iterator<Args...> &other) {
+    return !(*this == other);
+  }
+  constexpr inline auto operator*() {
+    return std::apply([](auto &&... args) { return std::tuple(*args...); },
+                      its);
+  }
+};
 
-    template <class... Args> struct Zip {
-        static_assert(sizeof...(Args) >= 1, "Zip requires at least 1 argument");
-        using iterator = Iterator<typename std::decay_t<Args>::iterator...>;
-        iterator _begin;
-        iterator _end;
-        Zip(Args &&... args)
-            : _begin(std::begin(std::forward<Args>(args))...),
-            _end(std::end(std::forward<Args>(args))...) {}
+template <class... Args> struct Zip {
+  static_assert(sizeof...(Args) >= 1, "Zip requires at least 1 argument");
+  using iterator = Iterator<typename std::decay_t<Args>::iterator...>;
+  iterator _begin;
+  iterator _end;
+  Zip(Args &&... args)
+      : _begin(std::begin(std::forward<Args>(args))...),
+        _end(std::end(std::forward<Args>(args))...) {}
 
-        constexpr inline auto begin() { return _begin; }
-        constexpr inline auto end() { return _end; }
-        constexpr inline auto begin() const { return _begin; }
-        constexpr inline auto end() const { return _end; }
-    };
+  constexpr inline auto begin() { return _begin; }
+  constexpr inline auto end() { return _end; }
+  constexpr inline auto begin() const { return _begin; }
+  constexpr inline auto end() const { return _end; }
+};
 
-    template <class... Args> Zip(Args &&...) -> Zip<Args...>;
+template <class... Args> Zip(Args &&...) -> Zip<Args...>;
 
+struct IndexIterator {
+  int i = 0;
+  constexpr IndexIterator(int i) : i(i) {}
+  constexpr int size() const { return std::numeric_limits<int>::max(); }
+  using iterator = IndexIterator;
+  IndexIterator &operator++() {
+    ++i;
+    return *this;
+  }
+  int &operator*() { return i; }
+  bool operator==(const IndexIterator &other) const { return i == other.i; }
+  bool operator!=(const IndexIterator &other) const {
+    return !(*this == other);
+  }
+  constexpr auto &begin() { return *this; }
+  constexpr auto end() { return IndexIterator{size()}; }
+  constexpr auto &begin() const { return *this; }
+  constexpr auto end() const { return IndexIterator{size()}; }
+};
 
-    struct IndexIterator {
-        int i = 0;
-        constexpr IndexIterator(int i) : i(i) {}
-        constexpr int size() const { return std::numeric_limits<int>::max(); }
-        using iterator = IndexIterator;
-        IndexIterator &operator++() {
-            ++i;
-            return *this;
-        }
-        int &operator*() {
-            return i;
-        }
-        bool operator==(const IndexIterator &other) const { return i == other.i; }
-        bool operator!=(const IndexIterator &other) const { return !(*this == other); }
-        constexpr auto &begin() { return *this; }
-        constexpr auto end() { return IndexIterator{size()}; }
-        constexpr auto &begin() const { return *this; }
-        constexpr auto end() const { return IndexIterator{size()}; }
-    };
-
-    template <class T> auto enumerate(T &&t) { return Akura::Zip(std::forward<T>(t), IndexIterator{0}); }
+template <class T> auto enumerate(T &&t) {
+  return Akura::Zip(std::forward<T>(t), IndexIterator{0});
+}
 
 }; // namespace Akura
--- tests/zip/zip.cpp
+++ tests/zip/zip.cpp
@@ -4,91 +4,92 @@
 
 using namespace Akura;
 
-template <class F, class... Args>
-static void loop(F && f, Args &&... args) {
-    auto size = std::min({args.size()...});
-    size_t iterations = 0;
-    for (auto && tup : Zip(args...)) {
-        ++iterations;
-        std::apply(f, tup);
-    }
-    REQUIRE(size == iterations);
+template <class F, class... Args> static void loop(F &&f, Args &&... args) {
+  auto size = std::min({args.size()...});
+  size_t iterations = 0;
+  for (auto &&tup : Zip(args...)) {
+    ++iterations;
+    std::apply(f, tup);
+  }
+  REQUIRE(size == iterations);
 }
 
 TEST_CASE("Zip empty container", "[zip]") {
-    std::vector<int> v{};
-    REQUIRE_NOTHROW(loop([](auto && a){ throw; }, v));
+  std::vector<int> v{};
+  REQUIRE_NOTHROW(loop([](auto &&a) { throw; }, v));
 }
 
 TEST_CASE("Zip same size", "[zip]") {
-    std::array arr1{1, 2, 3};
-    std::array arr2{1, 2, 3};
-    int i = 0;
-    loop([](auto && a, auto && b){ REQUIRE(a == b); }, arr1, arr2);
+  std::array arr1{1, 2, 3};
+  std::array arr2{1, 2, 3};
+  int i = 0;
+  loop([](auto &&a, auto &&b) { REQUIRE(a == b); }, arr1, arr2);
 }
 
 TEST_CASE("Zip with itself", "[zip]") {
-    std::vector v{1, 2, 3};
-    int i = 0;
-    for (auto &&[a, b] : Zip(v, v)) {
-        ++i;
-        REQUIRE(a == b);
-    }
-    REQUIRE(i == v.size());
+  std::vector v{1, 2, 3};
+  int i = 0;
+  for (auto &&[a, b] : Zip(v, v)) {
+    ++i;
+    REQUIRE(a == b);
+  }
+  REQUIRE(i == v.size());
 }
 
 TEST_CASE("Zip different types", "[zip]") {
-    std::vector v{1, 2, 3};
-    std::vector arr{1, 2, 3};
-    int i = 0;
-    for (auto &&[a, b] : Zip(v, v)) {
-        ++i;
-        REQUIRE(a == b);
-    }
-    REQUIRE(i == v.size());
+  std::vector v{1, 2, 3};
+  std::vector arr{1, 2, 3};
+  int i = 0;
+  for (auto &&[a, b] : Zip(v, v)) {
+    ++i;
+    REQUIRE(a == b);
+  }
+  REQUIRE(i == v.size());
 }
 
 TEST_CASE("Enumerate", "[zip]") {
-    std::vector v{0, 2, 4, 6, 8, 10};
-    int i = 0;
-    for (auto &&[item, index] : enumerate(v)) {
-        REQUIRE(index == i);
-        REQUIRE(item == index * 2);
-        ++i;
-    }
-    REQUIRE(i == v.size());
+  std::vector v{0, 2, 4, 6, 8, 10};
+  int i = 0;
+  for (auto &&[item, index] : enumerate(v)) {
+    REQUIRE(index == i);
+    REQUIRE(item == index * 2);
+    ++i;
+  }
+  REQUIRE(i == v.size());
 }
 
 TEST_CASE("Enumerate empty container", "[zip]") {
-    std::vector<int> v{};
-    REQUIRE_NOTHROW([&]() {
-            for (auto &&[item, index] : enumerate(v)) { throw; }
-    });
+  std::vector<int> v{};
+  REQUIRE_NOTHROW([&]() {
+    for (auto &&[item, index] : enumerate(v)) {
+      throw;
+    }
+  });
 }
 
 TEST_CASE("Zip a zip", "[zip]") {
-    std::vector<int> v{0, 1, 2};
-    for (auto &&[item, enumeration] : Zip(v, enumerate(v))) {
-        auto & [other_item, i] = enumeration;
-        REQUIRE(item == i);
-    }
+  std::vector<int> v{0, 1, 2};
+  for (auto &&[item, enumeration] : Zip(v, enumerate(v))) {
+    auto &[other_item, i] = enumeration;
+    REQUIRE(item == i);
+  }
 }
 
 TEST_CASE("Benchmark zip vs raw", "[zip]") {
-    std::array arr1{1, 2, 3};
-    std::array arr2{1, 2, 3};
-    auto size = std::min(arr1.size(), arr2.size());
-    bool ret = false;
-    BENCHMARK("raw loop") { 
-        for (int i = 0; i < size; ++i) {
-            ret = arr1[i] == arr2[i];
-        }
-        return ret;
-    };
-    BENCHMARK("zip loop") { 
-        for (auto && [item1, item2] : Zip(arr1, arr2)) {
-            ret = item1 == item2;
-        }
-        return ret;
-    };
+  std::array arr1{1, 2, 3};
+  std::array arr2{1, 2, 3};
+  auto size = std::min(arr1.size(), arr2.size());
+  bool ret = false;
+  BENCHMARK("raw loop") {
+    for (int i = 0; i < size; ++i) {
+      ret = arr1[i] == arr2[i];
+    }
+    return ret;
+  };
+  BENCHMARK("zip loop") {
+    for (auto &&[item1, item2] : Zip(arr1, arr2)) {
+      ret = item1 == item2;
+    }
+    return ret;
+  };
 }
